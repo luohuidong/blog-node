164af9661bb0134d5147bef7e1379d9ad08dc50e
luohuidong <luohuidong01@126.com>
2019-03-23 21:29:56 +0800

    完善路由处理
    完善博客接口与用户登录的路由处理

--- src/router/blog.js
+++ src/router/blog.js
@@ -1,34 +1,53 @@
+const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
 const handleBlogRouter = (req, res) => {
   const method = req.method
   const path = req.path
+  const blogId = req.query.id
 
   // 获取博客列表
   if (method === 'GET' && path === '/api/blog/list') {
-    return {
-      msg: '这是获取博客列表的接口'
-    }
+    const authod = req.query.authod || ''
+    const keyword = req.query.keyword || ''
+
+    const listData = getList(authod, keyword)
+    return new SuccessModel(listData)
   }
 
   // 获取博客详情
   if (method === 'GET' && path === '/api/blog/detail') {
-    return {
-      msg: '博客详情接口'
-    }
+    const data = getDetail(blogId)
+    return new SuccessModel(data)
   }
 
   // 新建一篇博客
   if (method === 'POST' && path === '/api/blog/new') {
-    return {
-      msg: '新增博客接口'
+    const data = newBlog(req.body)
+    return new SuccessModel(data)
+  }
+
+  // 更新一篇博客
+  if (method === 'POST' && path === '/api/blog/update') {
+    const result = updateBlog(blogId, req.body) 
+
+    if (result) {
+      return new SuccessModel()
+    } else {
+      return new ErrorModel('更新博客失败')
     }
   }
 
   // 删除博客
   if (method === 'POST' && path === '/api/blog/del') {
-    return {
-      msg: '删除博客接口'
+    const result = delBlog(blogId)
+    if (result) {
+      return new SuccessModel()
+    } else {
+      return new ErrorModel('删除博客失败')
     }
   }
+ 
 }
 
 module.exports = handleBlogRouter
--- src/router/user.js
+++ src/router/user.js
@@ -1,12 +1,19 @@
+const { logincheck } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
 const handleUserRouter = (req, res) => {
   const method = req.method
   const path = req.path
 
   // 登录
   if (method === 'POST' && path === '/api/user/login') {
-    return {
-      msg: '登录接口'
+    const { username, password } = req.body
+    const result = logincheck(username, password)
+
+    if (result) {
+      return new SuccessModel('登录成功')
     }
+    return new ErrorModel('登录失败')
   }
 }
 
