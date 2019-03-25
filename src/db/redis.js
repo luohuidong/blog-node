bb7d9a581b8bc86a850aa478d47a42a4707e4f2f
luohuidong <luohuidong01@126.com>
2019-03-25 23:39:45 +0800

    新增 redis 操作

---
+++ src/db/redis.js
@@ -0,0 +1,46 @@
+const redis = require('redis')
+const { REDIS_CONF } = require('../conf/db')
+
+const { host, port } = REDIS_CONF
+
+// 创建客户端
+const client = redis.createClient({ host, port })
+
+client.on('error', err => {
+  console.log('err', err)
+})
+
+function set(key, val) {
+  if (typeof val === 'object') {
+    // 如果 val 是一个对象，则将它转化为 JSON 进行存储
+    val = JSON.stringify(val)
+  }
+  client.set(key, val, redis.print)
+}
+
+function get(key) {
+  return new Promise((resolve, reject) => {
+    client.get(key, (err, val) => {
+      if (err) {
+        reject(err)
+      }
+
+      if (val == null) {
+        resolve(null)
+      }
+
+      try {
+        // 如果是一个 Json，则对它进行解析
+        resolve(JSON.parse(val))
+      } catch (error) {
+        // 否则直接返回 value
+        resolve(val)
+      }
+    })
+  })
+}
+
+module.exports = {
+  set,
+  get
+}
