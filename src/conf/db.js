ef2173f4e49f811428f266a58735ce84bd29a940
luohuidong <luohuidong01@126.com>
2019-03-25 23:38:12 +0800

    添加 redis 配置

--- src/conf/db.js
+++ src/conf/db.js
@@ -4,20 +4,30 @@ const {
 } = process.env
 
 let MYSQL_CONF
+let REDIS_CONF
 
 if (env === 'dev') {
+  // MySQL
   MYSQL_CONF = {
     host: DEV_DB_HOST,
     user: DEV_DB_USER,
     password: DEV_DB_PASSWORD,
     database: DEV_DB_DATABASE
   }
+
+  // Redis
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1'
+  }
 }
 
 if (env === 'production') {
-
+  // MySQL..
+  // Redis..
 }
 
 module.exports = {
-  MYSQL_CONF
+  MYSQL_CONF,
+  REDIS_CONF
 }
