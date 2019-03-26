a933fa60daa2f31ce1e6498e937d97c0694a4a80
luohuidong <luohuidong01@126.com>
2019-03-26 17:52:42 +0800

    修改登录校验

--- src/router/blog.js
+++ src/router/blog.js
@@ -3,9 +3,13 @@ const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 // 登录验证函数
 const loginCheck = req => {
-  if (!req.session.username) {
-    return Promise.resolve(new ErrorModel('未登录'))
-  }
+  return new Promise((resolve, reject) => {
+    if (req.session.username) {
+      resolve()
+    } else {
+      reject('用户未登录')
+    }
+  })
 }
 
 const handleBlogRouter = (req, res) => {
@@ -32,49 +36,33 @@ const handleBlogRouter = (req, res) => {
 
   // 新建一篇博客
   if (method === 'POST' && path === '/api/blog/new') {
-    const loginCheckResult = loginCheck(req)
-    if (loginCheckResult) {
-      // 未登录
-      return loginCheck
-    }
-
-    req.body.author = req.session.username
-    return newBlog(req.body).then(result => {
-      return new SuccessModel(result)
+    return loginCheck(req).then((result) => {
+      req.body.author = req.session.username
+      return newBlog(req.body).then(result => {
+        return new SuccessModel(result)
+      })
     })
   }
 
   // 更新一篇博客
   if (method === 'POST' && path === '/api/blog/update') {
-
-    // 登录验证
-    const loginCheckResult = loginCheck(req)
-    if (loginCheckResult) {
-      // 未登录
-      return loginCheck
-    }
-
-    return updateBlog(blogId, req.body).then(result => {
-      if (result) {
-        return new SuccessModel('更新博客成功')
-      } else {
-        return new ErrorModel('更新博客失败')
-      }
-    })
+    return loginCheck(req)
+      .then(() => updateBlog(blogId, req.body))
+      .then(result => {
+        if (result) {
+          return new SuccessModel('更新博客成功')
+        } else {
+          return new ErrorModel('更新博客失败')
+        }
+      })
   }
 
   // 删除博客
   if (method === 'POST' && path === '/api/blog/del') {
-    // 登录验证
-    const loginCheckResult = loginCheck(req)
-    if (loginCheckResult) {
-      // 未登录
-      return loginCheck
-    }
-
-    const author = req.session.username
-
-    return delBlog(blogId, author).then(result => {
+    return loginCheck(req).then(() => {
+      const author = req.session.username
+      return delBlog(blogId, author)
+    }).then(result => {
       if (result) {
         return new SuccessModel('删除博客成功')
       } else {
