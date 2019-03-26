08394a6bfe49f320cf6a5255c2766b93ac2e0b1c
luohuidong <luohuidong01@126.com>
2019-03-26 17:53:18 +0800

    更新 Header 的设置

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ require('dotenv').config()
 const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
-const { get, set } = require('./src/db/redis')
-
-// let SESSION_DATA = {} // 该对象存储了所有用户的 session 数据
+const { redisGet, redisSet } = require('./src/db/redis')
+const { ErrorModel } = require('./src/model/resModel')
 
 const serverHandle = (req, res) => {
   // 设置 JSON 返回格式
@@ -22,24 +21,25 @@ const serverHandle = (req, res) => {
   const cookie = parseCookie(req)
   req.cookie = cookie
 
-
   // 解析 session
   let needSetCookie = false // 是否需要设置 Cookie
   let userId = req.cookie.userId // cookie 中是否存在 userId
   if (!userId) {
     // 如果 Cookie 中没有 userId, 则标记为需要设置 Cookie
-    // 生成一个 userId，并初始化 Redis 中 userId 对应的值
+    // 生成一个 userId
     needSetCookie = true
     userId = `${Date.now()}_${Math.random()}`
-    set(userId, {})
   }
-  req.sessionId = userId // 将 sessionId 设置为 userId
+
+  // 将 sessionId 设置为 userId，后面的登录路由处理会用到
+  req.sessionId = userId
+
   // 通过 userId 获取存储在 Redis 中的数据
-  get(userId).then(sessionData => {
+  redisGet(req.sessionId).then(sessionData => {
     if (sessionData === null) {
-      // 当对应的 userId 在 redis 中没有值的时候
-      // 则在 redis 中设置值
-      set(req.sessionId, {})
+      // 当对应的 sessionId 在 redis 中没有值的时候，
+      // 在 redis 中将其值设置为空对象
+      redisSet(req.sessionId, {})
       req.session = {}
     } else {
       req.session = sessionData
@@ -57,6 +57,8 @@ const serverHandle = (req, res) => {
           setCookie(res, userId)
         }
         res.end(JSON.stringify(blogData))
+      }).catch(err => {
+        res.end(JSON.stringify(new ErrorModel(err)))
       })
     }
 
@@ -137,12 +139,7 @@ function parseCookie(req) {
  * @param {string} userId
  */
 function setCookie(res, userId) {
-  res.setHeader('Set-Cookie', [
-    `userId=${userId}`,
-    'path=/',
-    'httpOnly',
-    `expires=${getCookieExpires()}`
-  ])
+  res.setHeader('Set-Cookie', `userId=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
 }
 
 /**
